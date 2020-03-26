854b78db8c2be76e78902288fba7514129074d10
Takamasa Oshikiri <mail@oshikiri.org>
2020-03-26 23:13:18 +0900

    Add comment for types

--- types.go
+++ types.go
@@ -2,6 +2,7 @@ package main
 
 // See https://hledger.org/add.html#what-s-in-a-transaction
 
+// Transaction contains its meta data and array of posting
 type Transaction struct {
 	date        string
 	status      string
@@ -9,11 +10,13 @@ type Transaction struct {
 	postings    []Posting
 }
 
+// Posting contains its account type and amount object
 type Posting struct {
 	account string
 	amount  Amount
 }
 
+// Amount contains amount of money and its currency
 type Amount struct {
 	figure   int
 	currency string
