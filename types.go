8512f94ce68a1320aea99f23b783523baa8534d2
Takamasa Oshikiri <mail@oshikiri.org>
2020-03-27 21:04:54 +0900

    Implement Transaction#calculateTotalAmount

--- types.go
+++ types.go
@@ -10,6 +10,14 @@ type Transaction struct {
 	postings    []Posting
 }
 
+func (tx *Transaction) calculateTotalAmount() map[string]int {
+	totalAmounts := map[string]int{}
+	for _, posting := range tx.postings {
+		totalAmounts[posting.currency] += posting.figure
+	}
+	return totalAmounts
+}
+
 // Posting contains its account type and amount object
 type Posting struct {
 	account  string
---
+++ types_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTransactionTotalAmount(t *testing.T) {
+	tx := transactionsImbalanced
+	actual := tx.calculateTotalAmount()
+	expected := map[string]int{
+		"JPY": -1800,
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("%v != %v", actual, expected)
+	}
+}
