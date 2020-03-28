f9e472b53005b9f008079272b4a9ad0b767814d6
Takamasa Oshikiri <mail@oshikiri.org>
2020-03-28 15:18:06 +0900

    Refactor types_test

--- types_test.go
+++ types_test.go
@@ -5,24 +5,41 @@ import (
 	"testing"
 )
 
-func TestTransactionTotalAmount(t *testing.T) {
-	tx := transactionsImbalanced
-	_, actual := tx.calculateTotalAmount()
-	expected := map[string]Amount{
-		"JPY": -1800,
-	}
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("%v != %v", actual, expected)
-	}
+type TransactionFixtureTuple struct {
+	transaction                    Transaction
+	expectedContainsOneEmptyAmount bool
+	expectedAmounts                map[string]Amount
 }
 
-func TestTransactionEmptyAmount(t *testing.T) {
-	tx := transactionsBalancedEmptyAmount
-	_, actual := tx.calculateTotalAmount()
-	expected := map[string]Amount{
-		"JPY": 800,
+func TestTransactionCalculateTotalAmount(t *testing.T) {
+	fixtures := []TransactionFixtureTuple{
+		{
+			transactionsBalancedEmptyAmount,
+			true,
+			map[string]Amount{
+				"JPY": 800,
+			},
+		},
+		{
+			transactionsImbalanced,
+			false,
+			map[string]Amount{
+				"JPY": -1800,
+			},
+		},
 	}
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("%v != %v", actual, expected)
+
+	for _, fixture := range fixtures {
+		transaction := fixture.transaction
+		expectedContainsOneEmptyAmount := fixture.expectedContainsOneEmptyAmount
+		expectedAmounts := fixture.expectedAmounts
+
+		actualContainsOneEmptyAmount, actualTotalAmounts := transaction.calculateTotalAmount()
+		if actualContainsOneEmptyAmount != expectedContainsOneEmptyAmount {
+			t.Errorf("containsOneEmptyAmount should be %v but got %v", expectedContainsOneEmptyAmount, actualContainsOneEmptyAmount)
+		}
+		if !reflect.DeepEqual(actualTotalAmounts, expectedAmounts) {
+			t.Errorf("%v != %v", actualTotalAmounts, expectedAmounts)
+		}
 	}
 }
