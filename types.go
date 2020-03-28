351510453a890dbeffb56b46fd56565203c6d07a
Takamasa Oshikiri <mail@oshikiri.org>
2020-03-28 10:45:07 +0900

    Add type comments

--- types.go
+++ types.go
@@ -2,8 +2,13 @@ package main
 
 // See https://hledger.org/add.html#what-s-in-a-transaction
 
+// Date is date string
 type Date string
+
+// TransactionStatus should be one of ['*', '!', '']
 type TransactionStatus string
+
+// Amount for transaction posting, without currency
 type Amount int
 
 // Transaction contains its meta data and array of posting
