d5eee1dc7a3cbd0a27ac6680895b741a282d653a
Takamasa Oshikiri <mail@oshikiri.org>
2020-04-02 00:31:53 +0900

    Refactor using Validator class

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"strings"
 )
 
@@ -20,34 +19,17 @@ func main() {
 	var accountsPath = flag.String("account", "", "known accounts file")
 	flag.Parse()
 
-	knownAccounts := []string{}
-	if *accountsPath != "" {
-		knownAccountsStr, _ := readFileContent(*accountsPath) // FIXME: error handling
-		knownAccounts = strings.Split(knownAccountsStr, "\n")
-	}
+	validator := newValidator(*filePath, *accountsPath)
 
 	countNewlines := 1
 	transactionsStr, _ := readFileContent(*filePath) // FIXME: error handling
 	for _, transactionStr := range strings.Split(transactionsStr, "\n\n") {
 		_, transaction := parseTransactionStr(transactionStr)
 
-		// Check the transaction is balanced or not
-		containsOneEmptyAmount, totalAmount := transaction.calculateTotalAmount()
-		if !(isZeroAmount(totalAmount) || containsOneEmptyAmount) {
-			imbalancedTransactionMsg := buildImbalancedTransactionMsg(
-				*filePath,
-				countNewlines,
-				totalAmount,
-			)
-			fmt.Println(imbalancedTransactionMsg)
-		}
+		validator.checkBalancing(countNewlines, transaction)
 
-		// Check unknown account
 		for i, posting := range transaction.postings {
-			if len(knownAccounts) > 0 && !contains(knownAccounts, posting.account) {
-				unknownAccountMsg := "%v:%v unknown account: %v\n"
-				fmt.Printf(unknownAccountMsg, *filePath, countNewlines+i+1, posting.account)
-			}
+			validator.checkUnknownAccount(countNewlines+i+1, posting)
 		}
 
 		countNewlines += strings.Count(transactionStr, "\n") + 2
---
+++ validator.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func newValidator(filePath, accountsPath string) *Validator {
+	validator := Validator{
+		filePath:     filePath,
+		accountsPath: accountsPath,
+	}
+
+	if accountsPath != "" {
+		knownAccountsStr, _ := readFileContent(accountsPath) // FIXME: error handling
+		validator.knownAccounts = strings.Split(knownAccountsStr, "\n")
+	}
+
+	return &validator
+}
+
+// Validator checks transaction and posting, and print warning message if error is found.
+type Validator struct {
+	filePath      string
+	accountsPath  string
+	knownAccounts []string
+}
+
+func (validator *Validator) checkUnknownAccount(countNewlines int, posting Posting) {
+	if len(validator.knownAccounts) > 0 && !contains(validator.knownAccounts, posting.account) {
+		unknownAccountMsg := "%v:%v unknown account: %v\n"
+		fmt.Printf(unknownAccountMsg, validator.filePath, countNewlines, posting.account)
+	}
+}
+
+func (validator *Validator) checkBalancing(countNewlines int, transaction Transaction) {
+	containsOneEmptyAmount, totalAmount := transaction.calculateTotalAmount()
+
+	if !(isZeroAmount(totalAmount) || containsOneEmptyAmount) {
+		imbalancedTransactionMsg := buildImbalancedTransactionMsg(
+			validator.filePath,
+			countNewlines,
+			totalAmount,
+		)
+		fmt.Println(imbalancedTransactionMsg)
+	}
+}
