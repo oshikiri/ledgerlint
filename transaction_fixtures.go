91d148ce2581304742dda167fdd9c325620952e4
Takamasa Oshikiri <mail@oshikiri.org>
2020-05-31 16:06:33 +0900

    Revert "Remove transaction_fixtures.go"
    This reverts commit 6e5da7dc7d2fafaed0a55e7e73a7019a7bca70ae.

---
+++ transaction_fixtures.go
@@ -0,0 +1,103 @@
+package main
+
+var transactionsBalanced = Transaction{
+	date:        "2020-03-26",
+	status:      "*",
+	description: "super market",
+	postings: []Posting{
+		{
+			account:  "Expenses:Household essentials",
+			amount:   200,
+			currency: "JPY",
+		},
+		{
+			account:  "Expenses:Food",
+			amount:   600,
+			currency: "JPY",
+		},
+		{
+			account:  "Assets:Cash",
+			amount:   -800,
+			currency: "JPY",
+		},
+	},
+}
+
+var transactionsBalancedEmptyAmount = Transaction{
+	date:        "2020-03-26",
+	status:      "*",
+	description: "super market",
+	postings: []Posting{
+		{
+			account:  "Expenses:Household essentials",
+			amount:   200,
+			currency: "JPY",
+		},
+		{
+			account:  "Expenses:Food",
+			amount:   600,
+			currency: "JPY",
+		},
+		{
+			account:     "Assets:Cash",
+			emptyAmount: true,
+		},
+	},
+}
+
+var transactionsImbalanced = Transaction{
+	date:        "2020-03-26",
+	status:      "*",
+	description: "toilet paper",
+	postings: []Posting{
+		{
+			account:  "Expenses:Household essentials",
+			amount:   200,
+			currency: "JPY",
+		},
+		{
+			account:  "Assets:Cash",
+			amount:   -2000,
+			currency: "JPY",
+		},
+	},
+}
+
+var transactionsImbalancedMultiCurrency = Transaction{
+	date:        "2020-03-01",
+	status:      "!",
+	description: "at super market",
+	postings: []Posting{
+		{
+			account:  "Expenses:Food",
+			amount:   1000,
+			currency: "JPY",
+		},
+		{
+			account:  "Expenses:Household essentials",
+			amount:   200,
+			currency: "USD",
+		},
+		{
+			account:  "Assets:Cash",
+			amount:   -2000,
+			currency: "USD",
+		},
+	},
+}
+
+var transactionDollar = Transaction{
+	date: "2020/03/26",
+	postings: []Posting{
+		{
+			account:  "Expences:Household essentials",
+			amount:   1.23,
+			currency: "$",
+		},
+		{
+			account:  "Assets:Cash",
+			amount:   -1.24,
+			currency: "$",
+		},
+	},
+}
