b635ef12f49237f1f367a36da94ab9a353cbb7af
Takamasa Oshikiri <mail@oshikiri.org>
2020-05-31 16:01:04 +0900

    Move readFileContent

--- lint.go
+++ lint.go
@@ -1,8 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"strings"
 )
+
+func readFileContent(filePath string) (string, error) {
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Printf("ioutil.ReadFile failed: %v, filePath='%v'\n", err, filePath)
+		return "", err
+	}
+	fileContent := string(bytes)
+	return fileContent, nil
+}
 
 func lintTransactionFile(filePath, accountsPath string, outputJSON bool) {
 	transactionsStr, err := readFileContent(filePath)
