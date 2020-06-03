7e93c3b7c8f73b959d01d76c0260e684e457b423
Takamasa Oshikiri <mail@oshikiri.org>
2020-06-03 22:26:12 +0900

    Implement -v option

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"flag"
+	"fmt"
 )
+
+var version = "unspecified" // filled by ldflags option
 
 func main() {
 	var filePath = flag.String("f", "", "ledger/hledger transaction file")
 	var accountsPath = flag.String("account", "", "known accounts file")
 	var outputJSON = flag.Bool("j", false, "output error message by JSON format or plaintext (default plaintext)")
+	var showVersion = flag.Bool("v", false, "show version and exit")
 	flag.Parse()
+
+	if *showVersion {
+		fmt.Println(version)
+		return
+	}
 
 	if *filePath == "" {
 		panic("file path is empty. specify transaction file using '-f' option.")
