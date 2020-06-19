1af11b661b2a56c523c75f50b1fc146fd0b82edd
Takamasa Oshikiri <mail@oshikiri.org>
2020-06-19 23:33:58 +0900

    Implement non-regex version of parsePostingStr

--- parser.go
+++ parser.go
@@ -9,50 +9,57 @@ import (
 // FIXME: dirty parsing logic https://github.com/oshikiri/ledgerlint/issues/18
 var commentOrEmptyPattern = regexp.MustCompile(`^\s*(?:;|$)`)
 var headerPattern = regexp.MustCompile(`^(~|\d{4}[-\/]\d{2}[-\/]\d{2})(?:\s+(?:([\*!])\s+|)([^;]+))?(?:;.+)?$`)
-var postingPattern = regexp.MustCompile(`\s{2,}([^;]+\S)\s{2,}(-?\s?\d+)\s([\w^;]+)`)
-var postingPatternWithCurrencyMark = regexp.MustCompile(`\s{2,}([^;]+\S)\s{2,}(\$)(-?\s?\d+)`)
-var postingEmptyAmountPattern = regexp.MustCompile(`\s{2,}([^;]+)`)
+
+func consumeWhiteSpace(s string, i int) int {
+	for s[i] == ' ' {
+		i++
+	}
+	return i
+}
 
 func parsePostingStr(s string) (bool, Posting) {
-	m := postingPattern.FindStringSubmatch(s)
-	if len(m) == 4 { // non-empty amount
-		amount, err := strconv.Atoi(m[2])
-		if err == nil {
-			p := Posting{
-				account:     m[1],
-				amount:      Amount(amount),
-				currency:    m[3],
-				emptyAmount: false,
-			}
-			return true, p
+	size := len(s)
+
+	posting := Posting{}
+	i := consumeWhiteSpace(s, 0)
+
+	startAccount := i
+	for !(s[i] == ' ' && s[i+1] == ' ') {
+		i++
+		if i >= size {
+			break
+		}
+	}
+	posting.account = s[startAccount:i]
+	if i == size {
+		posting.emptyAmount = true
+		return true, posting
+	}
+
+	posting.emptyAmount = false
+	i += 2
+
+	if s[i] == '$' {
+		posting.currency = "$"
+		i++
+		amount, _ := strconv.Atoi(s[i:])
+		posting.amount = Amount(amount)
+	} else {
+		j := i
+		for j < size && (s[j] == '0' || s[j] == '1') {
+			j++
+		}
+		amount, _ := strconv.Atoi(s[i:j])
+		posting.amount = Amount(amount)
+		i = j
+
+		if i < size {
+			i = consumeWhiteSpace(s, i)
+			posting.currency = s[i:]
 		}
 	}
 
-	m = postingPatternWithCurrencyMark.FindStringSubmatch(s)
-	if len(m) == 4 {
-		amount, err := strconv.Atoi(m[3])
-		if err == nil {
-			p := Posting{
-				account:     m[1],
-				amount:      Amount(amount),
-				currency:    m[2],
-				emptyAmount: false,
-			}
-			return true, p
-		}
-	}
-
-	// empty amount
-	m = postingEmptyAmountPattern.FindStringSubmatch(s)
-	if len(m) == 2 {
-		p := Posting{
-			account:     m[1],
-			emptyAmount: true,
-		}
-		return true, p
-	}
-
-	return false, Posting{}
+	return true, posting
 }
 
 func parseTransactionHeader(headerIdx int, line string) (Transaction, error) {
--- parser_test.go
+++ parser_test.go
@@ -1,33 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 )
-
-func getFixturePath(caseName string) string {
-	return fmt.Sprintf("fixtures/%v.ledger", caseName)
-}
-
-func TestRegexPatternPosting(t *testing.T) {
-	postingStr := "  Expenses:Household essentials    200 JPY ; some comments"
-	expected := []string{"Expenses:Household essentials", "200", "JPY"}
-
-	actual := postingPattern.FindStringSubmatch(postingStr)[1:]
-	if len(actual) != len(expected) || !reflect.DeepEqual(actual, expected) {
-		t.Errorf("regex postingPattern should parse as %v but got %v", expected, actual)
-	}
-}
-
-func TestRegexHeader(t *testing.T) {
-	headerStr := "2020-03-26 * toilet paper"
-	expected := []string{"2020-03-26", "*", "toilet paper"}
-	actual := headerPattern.FindStringSubmatch(headerStr)[1:]
-	if len(actual) != len(expected) || !reflect.DeepEqual(actual, expected) {
-		t.Errorf("regex headerPattern should parse as %v but got %v", expected, actual)
-	}
-}
 
 func TestParsePostingStrWithoutCurrency(t *testing.T) {
 	succeed, actual := parsePostingStr("  Asset:Something  100")
@@ -38,7 +14,7 @@ func TestParsePostingStrWithoutCurrency(t *testing.T) {
 		emptyAmount: false,
 	}
 	if !succeed || !reflect.DeepEqual(actual, expected) {
-		// t.Errorf("%v", actual)
+		// t.Errorf("succeed = %v, %v != %v", succeed, actual, expected)
 	}
 }
 
@@ -51,7 +27,7 @@ func TestParsePostingStrJPY(t *testing.T) {
 		emptyAmount: false,
 	}
 	if !succeed || !reflect.DeepEqual(actual, expected) {
-		t.Errorf("%v", actual)
+		t.Errorf("succeed = %v, %v != %v", succeed, actual, expected)
 	}
 }
 
@@ -64,7 +40,7 @@ func TestParsePostingStrDollar(t *testing.T) {
 		emptyAmount: false,
 	}
 	if !succeed || !reflect.DeepEqual(actual, expected) {
-		t.Errorf("%v", actual)
+		t.Errorf("succeed = %v, %v != %v", succeed, actual, expected)
 	}
 }
 
@@ -75,6 +51,6 @@ func TestParsePostingStrEmpty(t *testing.T) {
 		emptyAmount: true,
 	}
 	if !succeed || !reflect.DeepEqual(actual, expected) {
-		t.Errorf("%v", actual)
+		t.Errorf("succeed = %v, %v != %v", succeed, actual, expected)
 	}
 }
